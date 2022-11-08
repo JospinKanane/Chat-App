493044f739981829afe4f485dc4d0150099607f7
Jospin Kanane <jospinkanane@yahoo.com>
2022-11-08 14:45:59 +0200

    Recuperation des etatt sur les formunaire react

--- geechat/src/components/auth/Login.js
+++ geechat/src/components/auth/Login.js
@@ -1,19 +1,51 @@
 import React from 'react'
 
-function Auth() {
-  // const [name, setName] = useState('');
-  // const [email, setEmail] = useState('');
+function Auth({email, password, setEmail, setPassword}) {
 
+  const handleMailChange = (e) => {
+    setEmail(email =  e.target.value)
+
+  };
+
+  const handlePWChange = (e) => {
+    setPassword(password =  e.target.value)
+  };
 
   return (
     <div className='loginPage'>
         <h1>Login to chatApp</h1>
-        <form className='loginForm' method='POST' action='/login'>
-           <label className='email' for='loginEmail'>Email</label>
-           <input type='text' id='loginEmail' className="inputName" placeholder='Enter a your email here'/>
-           <label className='password' for='loginPassword'>Password</label>
-           <input type='password' id='loginPassword' className="inputPassword" placeholder='Enter your password here'/>
-           <button className='loginBTN'>Start Chatting</button>
+        <form 
+        className='loginForm' 
+        method='POST' 
+        action='/login'>
+           <label 
+           className='email' 
+           for='loginEmail'>
+           Email
+           </label>
+           <input 
+           type='email' 
+           id='loginEmail' 
+           className="inputEmail" 
+           placeholder='Enter a your email here'
+           onChange={handleMailChange}
+           />
+           <label 
+           className='password'
+           for='loginPassword'>
+           Password
+           </label>
+           <input 
+           type='password' 
+           id='loginPassword' 
+           className="inputPassword" 
+           placeholder='Enter your password here'
+           onChange={handlePWChange}
+           />
+           <button 
+           className='loginBTN'>
+           Start Chatting
+           </button>
         </form>
     </div>
   )
--- geechat/src/components/auth/Register.js
+++ geechat/src/components/auth/Register.js
@@ -1,21 +1,83 @@
 import React from 'react'
 
-function Register() {
-  // const [name, setName] = useState('');
-  // const [email, setEmail] = useState('');
+function Register({name, email, password, setName, setPassword, setEmail}) {
+
+  // const errName = document.querySelector('.errName')
+
+  const handleNameChange = (e) => {
+    setName(name = e.target.value); 
+  };
+  const handleMailChange = (e) => {
+    setEmail(email =  e.target.value)
+
+  };
+  const handlePWChange = (e) => {
+    setPassword(password =  e.target.value);
+  };
+
+  // const registration = (e) => {
+  //   e.preventDefault();
+  //   errName.innerContent = "Veillez enter un nom valable !"
+  // }
 
 
   return (
     <div className='signPage'>
         <h1>SignUp to chatApp</h1>
-        <form className='signForm' method='POST' action='/signin'>
-           <label className='name' for='signName'>Name</label>
-           <input type='text' id='signName' className="inputName" placeholder='Enter a your name or your username here'/>
-           <label className='email' for='signEmail'>Email</label>
-           <input type='text' id='signEmail' className="inputName" placeholder='Enter a your email here'/>
-           <label className='password' for='signPassword'>Password</label>
-           <input type='password' id='signPassword' className="inputPassword" placeholder='Enter your password here'/>
-           <button className='signBTN'>signUp</button>
+        <form 
+        className='signForm' 
+        method='POST' 
+        action='/signin'
+        >
+           <label 
+           className='name' 
+           for='signName'>
+           Name
+           </label>
+           <input 
+           type='text' 
+           id='signName' 
+           className="inputName" 
+           placeholder='Enter a your name or your username here'
+           onChange={handleNameChange}
+           />
+           {/* <span 
+           className='errName' 
+           style={{color: "red"}}>
+           </span> */}
+           <label 
+           className='email' 
+           for='signEmail'>
+           Email
+           </label>
+           <input type='text' id='signEmail' className="inputName" placeholder='Enter a your email here'
+           onChange={handleMailChange}
+           />
+           {/* <span 
+           className='errMail' 
+           style={{color: "red"}}>
+           </span> */}
+           <label 
+           className='password' 
+           for='signPassword'>
+           Password
+           </label>
+           <input 
+           type='password' 
+           id='signPassword' 
+           className="inputPassword" 
+           placeholder='Enter your password here'
+           onChange={handlePWChange}
+           />
+           {/* <span 
+           className='errPassword' 
+           style={{color: "red"}}>
+           </span> */}
+           <button 
+           className='signBTN'>
+           signUp
+           {/* onClick={registration} */}
+           </button>
         </form>
     </div>
   )
