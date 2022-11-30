65ed5b74cb039a02e0cc02b5da4578d86ff9e8d6
Jospin Kanane <jospinkanane@yahoo.com>
2022-11-30 08:45:18 +0200

    Ajout du middleware pour rechercher un seul utilisateur grace à son id

--- geechatApi/Routes/Router.js
+++ geechatApi/Routes/Router.js
@@ -8,6 +8,7 @@ router.post('/conversation', controller.conversation)
 router.get('/conversation/:userId', controller.getConversation)
 router.post('/messages', controller.messages)
 router.get('/messages/:conversationId', controller.getMessages)
-router.get('/user', controller.getUser)
+router.post('/user', controller.getUsers)
+router.get('/user/:userId', controller.getOneUser)
 
 module.exports = router;
--- geechatApi/server.js
+++ geechatApi/server.js
@@ -15,9 +15,9 @@ mongoose.connect( process.env.MONGODB_URI,
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+app.use(cors());
 app.use(express.json());
 
-app.use(cors());
 app.use(express.urlencoded({extended : false}))
 
 app.use(Routes)
