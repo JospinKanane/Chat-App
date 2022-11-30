bf76bc59af8a0f1bc7341a1ebef0516bce275cdb
Jospin Kanane <jospinkanane@yahoo.com>
2022-11-30 12:34:04 +0200

    Modification sur le middleware de get users

--- geechatApi/Routes/Router.js
+++ geechatApi/Routes/Router.js
@@ -8,7 +8,7 @@ router.post('/conversation', controller.conversation)
 router.get('/conversation/:userId', controller.getConversation)
 router.post('/messages', controller.messages)
 router.get('/messages/:conversationId', controller.getMessages)
-router.post('/user', controller.getUsers)
-router.get('/user/:userId', controller.getOneUser)
+router.get('/users', controller.getUsers)
+router.get('/users/:userId', controller.getOneUser)
 
 module.exports = router;
